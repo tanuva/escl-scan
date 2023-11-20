38da05b4aebf48e231a9f9049a806e0e52f54170
Marcel Kummer <tanuva@posteo.de>
2023-11-20 18:52:59 +0100

    Switch argument parser to clap's derive method
    I like the derive method better because it puts arguments directly into
    a neatly organized struct. Annotations make it clear what goes where
    in one place and I need fewer helper variables when working with the
    argument values.

--- escl-scan-cli/src/main.rs
+++ escl-scan-cli/src/main.rs
@@ -2,56 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#[macro_use]
 extern crate clap;
 extern crate scan;
 
-use clap::{Arg, Command};
+use clap::Parser;
 use std::path::Path;
 use std::process::exit;
 
+#[derive(clap::Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Enables overwriting the output file if it already exists
+    #[arg(short = 'f', long = "force")]
+    overwrite: bool,
+
+    /// Scan resolution in DPI (Dots Per Inch)
+    #[arg(short, long, default_value = "75")]
+    dpi: i16,
+
+    /// IP or hostname of the scanner
+    #[arg(value_name = "SCANNER_IP")]
+    ip: String,
+
+    // , default_value = "scan.jpg"
+    /// Output file name
+    #[arg(value_name = "OUTPUT_FILE_NAME")]
+    output_file_name: String,
+}
+
 fn main() {
     env_logger::init();
+    let args = Args::parse();
+    let scanner_base_path = format!("http://{}:80/eSCL", args.ip);
 
-    let matches = Command::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg_required_else_help(true)
-        .arg(Arg::new("ip").help("IP of scanner").index(1).required(true))
-        .arg(
-            Arg::new("destination file")
-                .help("Destination file")
-                .index(2)
-                .required(true),
-        )
-        .arg(
-            Arg::new("dpi")
-                .short('d')
-                .long("dpi")
-                .help("Scan resolution")
-                .default_value("75")
-                .value_parser(value_parser!(i16)),
-        )
-        .arg(
-            Arg::new("force")
-                .short('f')
-                .long("force")
-                .help("Force scan and override destination file"),
-        )
-        .get_matches();
-
-    let ip = matches.get_one::<String>("ip").unwrap();
-    let scanner_base_path = format!("http://{}:80/eSCL", ip);
-    let scan_resolution = matches.get_one::<i16>("dpi").unwrap();
-    let destination_file = matches.get_one::<String>("destination file").unwrap();
-
-    if !matches.contains_id("force") && Path::new(destination_file).exists() {
+    if !args.overwrite && Path::new(&args.output_file_name).exists() {
         eprintln!("Output file exists, exiting...");
         exit(1);
     }
 
-    if let Err(err) = scan::scan(&scanner_base_path, *scan_resolution, destination_file) {
+    if let Err(err) = scan::scan(&scanner_base_path, args.dpi, &args.output_file_name) {
         eprintln!("Failed to scan: {err:?}");
+        exit(1);
     }
 }
