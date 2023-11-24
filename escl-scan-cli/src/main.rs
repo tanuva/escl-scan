adb8284b9f3d1fa65ad6186d83ca9d5680adef64
Marcel Kummer <tanuva@posteo.de>
2023-11-24 21:51:44 +0100

    Refactor CLI arguments into device selection group
    Add a group of required arguments that select a scanner. The group will be the place to add
    - listing of discovered scanners
    - selecting a scanner by name instead of IP/hostname
    - showing information about a scanner

--- escl-scan-cli/src/main.rs
+++ escl-scan-cli/src/main.rs
@@ -5,14 +5,17 @@
 extern crate clap;
 extern crate scan;
 
-use clap::Parser;
+use clap::{Args, Parser};
 use scan::scanner::Scanner;
 use std::path::Path;
 use std::process::exit;
 
 #[derive(clap::Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
+struct Cli {
+    #[command(flatten)]
+    device: DeviceArgs,
+
     /// Enables overwriting the output file if it already exists
     #[arg(short = 'f', long = "force")]
     overwrite: bool,
@@ -21,40 +24,53 @@ struct Args {
     #[arg(short = 'r', long = "resolution", default_value = "75")]
     dpi: i16,
 
-    /// IP or hostname of the scanner
-    #[arg(long = "host", required = true)]
-    host: String,
-
-    /// Print scanner info and exit
-    #[arg(short, long)]
-    info: bool,
-
     /// Output file name
     #[arg(value_name = "OUTPUT_FILE_NAME", default_value = "scan.jpg")]
     output_file_name: String,
 }
 
+#[derive(Args)]
+#[group(required = true)]
+struct DeviceArgs {
+    /// Select scanner by IP or hostname
+    #[arg(long = "host")]
+    host: Option<String>,
+
+    /// Print information about the scanner identified by device name
+    #[arg(short, long)]
+    info: Option<String>,
+}
+
+fn get_scanner(cli: &Cli) -> Scanner {
+    if let Some(host) = &cli.device.host {
+        return Scanner::new(&host, None);
+    }
+
+    panic!("get_scanner called while no device was specified");
+}
+
 fn main() {
     env_logger::init();
-    let args = Args::parse();
+    let args = Cli::parse();
+
+    // TODO This is just a band-aid for testing
+    if let Some(name) = args.device.info {
+        exit(0);
+    }
+
+    let scanner = get_scanner(&args);
 
     if !args.overwrite && Path::new(&args.output_file_name).exists() {
         eprintln!("Output file exists, exiting...");
         exit(1);
     }
 
-    let scanner = Scanner::new(args.host, None);
     match scanner.get_status() {
         Ok(state) => println!("Scanner state: {state}"),
         Err(err) => {
             eprintln!("Failed to get status: {err:?}");
             exit(1);
         }
-    }
-
-    // TODO This is just a band-aid for testing
-    if args.info {
-        exit(0);
     }
 
     if let Err(err) = scanner.scan(args.dpi, &args.output_file_name) {
